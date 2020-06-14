4a9558fc1a6fadedada5d99815c98f8b75ca16cc
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-14 08:15:48 -0300

    I'm working with json

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,74 @@
 const chalk = require('chalk')
 const validator = require('validator')
-const add = require('./utils.js')
 const getNotes = require('./note.js')
+const yargs = require('yargs')
 
-console.log('this is app.js that call to utils.js')
-//console.log(name)
-console.log(add(7, 7))
-console.log(getNotes())
+//console.log(process.argv)
+//Customize yargs version
+yargs.version('1.1.0')
 
-console.log(validator.isEmail('nerius@gmail.example.com'))
-console.log(validator.isURL('hteodofof'))
-console.log(chalk.green('SUCCESS'), chalk.underline.bold.blue('hello'), chalk.inverse.yellow('It\'s yellow'))
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            description: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Adding a new note!!! ' + argv.title)
+        console.log('The body operator is: ' + argv.body)
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List a sets of elements',
+    handler: function () {
+        console.log('Listing a set of elements')
+    }
+})
+
+//Creat command to read
+yargs.command({
+    command: 'read',
+    describe: 'Read element to the console',
+    handler: function () {
+        console.log('Reading element to the consoles')
+    }
+})
+
+yargs.parse()
+
+//console.log(yargs.argv)
+//console.log(process.argv)
+
+
+
+/* console.log(process.argv)
+const command =process.argv[2]
+console.log(command)
+if(command === 'add'){
+    console.log('adding note')
+} else if (command === 'remove') {
+    console.log('removing information')
+} */
---
+++ notes-app/external-modules-app.js
@@ -0,0 +1,14 @@
+const chalk = require('chalk')
+const validator = require('validator')
+const add = require('./utils.js')
+const getNotes = require('./note.js')
+
+console.log('this is app.js that call to utils.js')
+//console.log(name)
+console.log(add(7, 7))
+console.log(getNotes())
+
+console.log(validator.isEmail('nerius@gmail.example.com'))
+console.log(validator.isURL('hteodofof'))
+console.log(chalk.green('SUCCESS'), chalk.underline.bold.blue('hello'), chalk.inverse.yellow('It\'s yellow'))
+console.log(process.argv[2])
