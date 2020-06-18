d7184b3e2095c59d7f3e8eec9be9a287d02643db
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-18 17:24:13 -0300

    Add new endpoints and renders short sections of html

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,12 +7,12 @@ const address = process.argv[2]
 if (!address) {
     console.log('It\'s not prvided the address!!!')
 } else {
-    geocode(address, (error, {latitude = 'latitude', longitude = 'longitude'} = {}) => {
+    geocode(address, (error, { latitude = 'latitude', longitude = 'longitude' } = {}) => {
         if (error) {
             return console.log(error)
         } else {
             forecast(latitude, longitude, (errorForesCast, weatherObject) => {
-                const {weather, country, locality, temperature, windChillFactor} = weatherObject
+                const { weather, country, locality, temperature, windChillFactor } = weatherObject
                 if (errorForesCast) {
                     return console.log(errorForesCast)
                 } else {
@@ -20,7 +20,7 @@ if (!address) {
                                  locality ${locality}. The temperature is: ${temperature} and the 
                                  windChillFactor ${windChillFactor}`)
                 }
-              })
+            })
         }
     })
 }
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,19 +3,29 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Hello express.')
+    res.send('<h1>Hello Word</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('help endpoint.')
+    res.send({
+        name: 'Nerius',
+        lastNema: 'Perez',
+        age: 37
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About title page.')
+    res.send('<h1>About Page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather page title.')
+    res.send({
+        weather: 'Partly cloudy',
+        country: 'Cuba',
+        locality: 'La Veguita',
+        temperature: 31,
+        windChillFactor: 39
+    })
 })
 
 //http://app.com
