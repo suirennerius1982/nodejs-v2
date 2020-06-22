e89e980568acceef1f079623499d0e7bfd732cbf
nerius.perez <ntoirac@proveedor.teco.com.ar>
2020-06-22 12:21:44 -0300

    Example the fetch api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,11 @@
 console.log('Javascript runnning in the client')
+
+fetch('http://localhost:3000/weather?address=sagua%tanamo%cuba').then((res) => {
+    res.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else (
+            console.log(data)
+        )
+    }) 
+})
